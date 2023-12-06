90ae78094ffd323bcd7769d7baeb6b62ca4bad84
GhifariRizkiRamadhan <144987480+GhifariRizkiRamadhan@users.noreply.github.com>
2023-12-06 20:13:19 +0700

    Add files via upload

---
+++ stringAndDateGhifari/src/dataPembelian.java
@@ -0,0 +1,22 @@
+
+public class dataPembelian implements cetak { // implement interface cetak
+    public String judul1 = "gip store";
+    public String judul2 = "data pelanggan";
+    public String judul3 = "data pembelian barang";
+    public String nofaktur;
+    public String namaPelanggan;
+    public String alamat;
+    public String noHP;
+    public String kodeBarang;
+    public String namaBarang;
+    public Integer hargaBarang;
+    public Integer jumlahBeli;
+    public Integer totalBayar;
+
+    public void isidata() {
+    }
+
+    @Override
+    public void cetakFaktur() {
+    }
+}
